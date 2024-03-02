5e714872b69fb1d8e1132b370f732fbe2b725156
risenfromashes <ashrafurkhan37@gmail.com>
2024-03-02 09:37:36 +0600

    add dinov2

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,71 @@
-
-import { createClient } from "@supabase/supabase-js";
-
+import { createClient } from '@supabase/supabase-js';
 
 export interface Env {
 	PUBLIC_SUPABASE_URL: string;
 	PUBLIC_SUPABASE_ANON_KEY: string;
 	ML_API_URL: string;
+	ML_API_URL2: string;
 	ML_AUTH_TOKEN: string;
 }
 
-const createEmbeddings = async (env: Env) => {
+const createEmbeddings = async (env: Env, model_: string) => {
+	const apiUrl: Map<string, string> = new Map();
+	apiUrl.set('clip', env.ML_API_URL);
+	apiUrl.set('dinov2', env.ML_API_URL2);
 	const supabase = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY);
-	const { data, error } = await supabase.rpc('get_images_without_embedding', { modelName: 'clip' });
+	let { data, error } = await supabase.rpc('get_images_without_embedding', { modelName: model_ });
 	if (error) {
 		console.error(error);
 		throw new Error(error.message);
 	}
 
-	const req = { images: data.map((d: any) => (d.url)) };
+	if (data.length === 0) {
+		return;
+	}
 
+	data = data.slice(0, Math.min(data.length, 20));
+	const req = { images: data.map((d: any) => d.url) };
+
+	const url = apiUrl.get(model_);
+	if (url === undefined) {
+		return;
+	}
 	try {
-		const res = await fetch(env.ML_API_URL, {
+		const res = await fetch(url, {
 			headers: {
 				Authorization: `Bearer ${env.ML_AUTH_TOKEN}`,
-				"Content-Type": "application/json"
-			}, method: 'POST', body: JSON.stringify(req)
+				'Content-Type': 'application/json',
+			},
+			method: 'POST',
+			body: JSON.stringify(req),
 		});
 
 		const json: any = await res.json();
 		console.log(json);
 
-		for (let i = 0; i < data.length; i++) {
+		for (let i = 0; i < json.images.length; i++) {
 			if (data[i].url === json.images[i].url) {
-				await supabase.from('ImageEmbedding').insert({ imageId: data[i].id, embedding: json.images[i].embedding, model: 'clip' });
+				await supabase.from('ImageEmbedding').insert({ imageId: data[i].id, embedding: json.images[i].embedding, model: model_ });
 			}
 		}
-
 	} catch (e: any) {
 		console.error(e);
 		throw new Error(e.message);
 	}
 };
 
-
 export default {
 	async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext): Promise<void> {
-		await createEmbeddings(env);
+		await createEmbeddings(env, 'clip');
+		await createEmbeddings(env, 'dinov2');
 	},
 	async fetch(event: FetchEvent, env: Env, ctx: ExecutionContext): Promise<Response> {
 		try {
-			await createEmbeddings(env);
+			await createEmbeddings(env, 'clip');
+			await createEmbeddings(env, 'dinov2');
 			return new Response('OK');
 		} catch (e: any) {
 			return new Response(e.message, { status: 500 });
 		}
-	}
+	},
 };
-
